9ca158b4aa31f1405b90ca13fb1cf5deaae0e496
EduardoDebarba <eduardodsdev@gmail.com>
2024-02-13 21:32:18 -0300

    add slides to my website section, in addition to creating media queries for this slide

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,15 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5000,
+       delay: 5000,
       disableOnInteraction: false,
-    },
+     },
     pagination: {
-      el: ".swiper-pagination",
+     el: ".swiper-pagination",
       clickable: true,
     },
-    loop:true
-  });
+ loop:true
+})
+
+
+
