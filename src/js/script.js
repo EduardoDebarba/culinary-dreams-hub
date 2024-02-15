29d6b5312ed0934e961e4b76f6eb05e2f2235446
EduardoDebarba <eduardodsdev@gmail.com>
2024-02-15 17:16:06 -0300

    add loader to page

--- src/js/script.js
+++ src/js/script.js
@@ -101,3 +101,13 @@ carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
 
+
+function loader(){
+   document.querySelector(".loader-container").classList.add('fade-out')
+}
+
+function fadeOut(){
+   setInterval(loader, 3000)
+}
+
+window.onload = fadeOut
